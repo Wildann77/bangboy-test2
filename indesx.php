2d8709e0d4ea4ab1336e6150eee58a2010496769
Wildann77 <boyblanco77@gmail.com>
2024-09-26 20:11:10 +0700

    nambah p

--- indesx.php
+++ indesx.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus facilis voluptatibus autem nobis enim culpa vel accusantium dolorum, libero cumque laborum, totam tenetur itaque tempore, labore possimus corporis eligendi. Perspiciatis!</p>
 </body>
 </html>
