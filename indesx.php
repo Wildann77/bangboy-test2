95c90e80c6476beb7f33fe2791580d1cf91d7e5c
Wildann77 <boyblanco77@gmail.com>
2024-09-26 20:17:19 +0700

    nambah list

--- indesx.php
+++ indesx.php
@@ -8,5 +8,8 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus facilis voluptatibus autem nobis enim culpa vel accusantium dolorum, libero cumque laborum, totam tenetur itaque tempore, labore possimus corporis eligendi. Perspiciatis!</p>
+    <li>
+        <ul>list/ul>
+    </li>
 </body>
 </html>
